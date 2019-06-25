11f42b07c5018b318ed581caa857832134e8937f
Luis Correa <luisc@take.net>
2019-06-25 14:33:41 -0300

    apple pay added

---
+++ AbcComponents/ApplePayDocument/BillTo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbcComponentsHelper.ApplePayDocument
+{
+    class BillTo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("taxDocument")]
+        public string TaxDocument { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("birthDate")]
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ AbcComponents/ApplePayDocument/Item.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbcComponentsHelper.ApplePayDocument
+{
+    class Item
+    {
+        [JsonProperty("unit")]
+        public decimal Unit { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("complement")]
+        public string Complement { get; set; }
+    }
+}
